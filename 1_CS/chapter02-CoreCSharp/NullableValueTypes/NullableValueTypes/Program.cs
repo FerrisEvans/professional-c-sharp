6b18683700709023f27d645cf6d276b1a7bb2fb8
小离 <438011578@qq.com>
2023-07-30 00:48:08 +0800

    chapter02 nullable value types

---
+++ 1_CS/chapter02-CoreCSharp/NullableValueTypes/NullableValueTypes/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int? n1 = null;
+
+int n2 = -1;
+
+if (n1.HasValue)
+{
+    n2 = n1.Value;
+}
+
+int n3 = 42;
+int? n4 = n3;
+
+Console.WriteLine("n1 = " + n1);
+Console.WriteLine("n2 = " + n2);
+Console.WriteLine("n3 = " + n3);
+Console.WriteLine("n4 = " + n4);
