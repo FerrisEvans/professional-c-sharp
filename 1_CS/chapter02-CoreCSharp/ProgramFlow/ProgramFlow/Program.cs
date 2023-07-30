78d8a2786a755ea3a06e16811263ec65d3078d95
小离 <438011578@qq.com>
2023-07-30 22:16:11 +0800

    p38

--- 1_CS/chapter02-CoreCSharp/ProgramFlow/ProgramFlow/Program.cs
+++ 1_CS/chapter02-CoreCSharp/ProgramFlow/ProgramFlow/Program.cs
@@ -1,3 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Type in a string");
+string? input = Console.ReadLine();
+
+if (string.IsNullOrEmpty(input))
+{
+    Console.WriteLine("You typed an empty string.");
+}
+else if (input?.Length < 5)
+{
+    Console.WriteLine("The string had lass than 5 characters.");
+}
+else
+{
+    Console.WriteLine("Read any other string.");
+}
+
+Console.WriteLine("The string was " + input);
 
