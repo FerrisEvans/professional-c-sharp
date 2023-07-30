1789b16fe7d700f35bbcf308f12403f8e70bad93
小离 <438011578@qq.com>
2023-07-30 22:26:05 +0800

    Update Program.cs

--- 1_CS/chapter02-CoreCSharp/SwitchStatement/SwitchStatement/Program.cs
+++ 1_CS/chapter02-CoreCSharp/SwitchStatement/SwitchStatement/Program.cs
@@ -1,3 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void SwitchSample(int x)
+{
+    switch (x)
+    {
+        case 1:
+            Console.WriteLine(1);
+            break;
+        case 2:
+            Console.WriteLine(2);
+            break;
+        case 3:
+            Console.WriteLine(3);
+            break;
+        default:
+            Console.WriteLine("-----");
+            break;
+    }
+}
 
+void SwitchWithPatternMatching(object o)
+{
+
+    switch (o)
+    {
+        case null:
+            Console.WriteLine("const pattern with null");
+            break;
+        case int i when i > 42:
+            Console.WriteLine("type pattern with when and a relational pattern");
+            break;
+        case int:
+            Console.WriteLine("type pattern with an int");
+            break;
+        case Book b:
+            Console.WriteLine($"type pattern with a Book {b?.Title}");
+            break;
+        default:
+            break;
+    }
+}
+
+class Book
+{
+    public string? Title { get; set; }
+}
