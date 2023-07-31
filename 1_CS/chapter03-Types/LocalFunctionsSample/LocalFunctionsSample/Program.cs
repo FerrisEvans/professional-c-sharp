bac522e949ce7fff8780e5c103090ab79cb9fa55
小离 <438011578@qq.com>
2023-07-31 23:12:54 +0800

    p70

---
+++ 1_CS/chapter03-Types/LocalFunctionsSample/LocalFunctionsSample/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static void IntroLocalFunctions()
+{
+    static int Add(int x, int y) => x + y;
+
+    int result = Add(3, 7);
+    Console.WriteLine($"called the local function with this result: {result}");
+
+}
+IntroLocalFunctions();
+
+static void LocalFunctionWithClosure()
+{
+    int z = 3;
+
+
+    int Add(int x, int y) => x + y + z;
+
+    int result = Add(3, 7);
+    Console.WriteLine($"called the local function with this result: {result}");
+}
+LocalFunctionWithClosure();
