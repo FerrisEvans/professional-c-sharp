341b005147e4072814c3218da3331da40116977e
小离 <438011578@qq.com>
2023-07-31 10:44:14 +0800

    p40

---
+++ 1_CS/chapter02-CoreCSharp/ForLoop/ForLoop/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+for (int i = 0; i < 100; i += 10)
+{
+    for (int j = i; j < i + 10; j++)
+    {
+        Console.Write($" {j}");
+    }
+    Console.WriteLine();
+}
+
+
