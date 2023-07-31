18e348db61f1ab66a9b70b451ab86453dd42d2ac
小离 <438011578@qq.com>
2023-07-31 20:26:56 +0800

    p63

---
+++ 1_CS/chapter03-Types/ClassesSample/ClassesSample/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public class Person
+{
+    private string _firstName;
+    private string _lastName;
+}
+
+public static class PeopleFactory
+{
+    private static int s_peopleCount;
+}
