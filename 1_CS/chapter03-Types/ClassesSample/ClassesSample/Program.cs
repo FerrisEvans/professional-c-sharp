8d4c9c59ccde7502dbb6f959374590086e9dd31f
小离 <438011578@qq.com>
2023-07-31 22:57:50 +0800

    p69

--- 1_CS/chapter03-Types/ClassesSample/ClassesSample/Program.cs
+++ 1_CS/chapter03-Types/ClassesSample/ClassesSample/Program.cs
@@ -3,11 +3,67 @@ Console.WriteLine("Hello, World!");
 
 public class Person
 {
-    private string _firstName;
-    private string _lastName;
+    private readonly string _firstName;
+    private readonly string _lastName;
+
+    public Person (string firstName, string lastName)
+    {
+        _firstName = firstName;
+        _lastName = lastName;
+    }
+
+    private int _age1;
+    public int Age1
+    {
+        get
+        {
+            return _age1;
+        }
+        set
+        {
+            _age1 = value;
+        }
+    }
+
+    private int _age;
+    public int Age
+    {
+        get => _age;
+        set => _age = value;
+    }
+
+    private string _name;
+    public string Name
+    {
+        get => _name;
+        private set => _name = value;
+    }
+
+    public string Name1 { get; private set; }
+
+    private readonly string _name2;
+    public string Name2
+    {
+        get => _name2;
+    }
+
+    public string Id { get; } = Guid.NewGuid().ToString();
 }
 
 public static class PeopleFactory
 {
     private static int s_peopleCount;
+}
+
+public class Book
+{
+    public string Title { get; }
+    public Book(string title) => Title = title;
+
+    public int No { get; init; }
+
+    public Book(int no)
+    {
+        No = no;
+    }
 }
---
+++ 1_CS/chapter03-Types/MathSample/MathSample/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+int x = Math.GetSquareOf(5);
+Console.WriteLine($"Square of 5 is {x}");
+
+Math m = new();
+m.Value = 30;
+Console.WriteLine($"Value field of math variable contains {m.Value}");
+Console.WriteLine($"Square of 30 is {m.GetSquare()}");
+
+void AnyNumberOfAtgs(params int[] data)
+{
+    foreach (var x in data)
+    {
+        Console.WriteLine(x);
+    }
+}
+
+AnyNumberOfAtgs();
+AnyNumberOfAtgs(1);
+AnyNumberOfAtgs(1, 2, 3, 4, 5);
+
+
+public class Math
+{
+    public int Value { get; set; }
+    public int GetSquare() => Value * Value;
+    public static int GetSquareOf(int x) => x * x;
+}
+
