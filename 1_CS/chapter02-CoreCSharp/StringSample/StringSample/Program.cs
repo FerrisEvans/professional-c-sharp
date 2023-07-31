0372076216fe1eecafa52eb6caaeb324b81a1c48
小离 <438011578@qq.com>
2023-07-31 12:24:46 +0800

    p45

---
+++ 1_CS/chapter02-CoreCSharp/StringSample/StringSample/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+void UsingStringBuilder()
+{
+    StringBuilder sb = new("the quick");
+    sb.Append(' ');
+    sb.Append("brown fox jumped over ");
+    sb.Append("the lazy dogs 12345678 times");
+
+    string s = sb.ToString();
+    Console.WriteLine(s);
+
+}
+UsingStringBuilder();
+
+
+void UsingFormattableString()
+{
+    int x = 3, y = 4;
+    FormattableString s = $"The result of {x} + {y} is {x + y}";
+    Console.WriteLine($"format: {s.Format}");
+
+    for (int i = 0; i < s.ArgumentCount; i++)
+    {
+        Console.WriteLine($"argument: {i}:{s.GetArgument(i)}");
+    }
+    Console.WriteLine();
+}
+UsingFormattableString();
+
+void UseStringFormat()
+{
+    DateTime day = new DateTime(2025, 2, 14);
+    Console.WriteLine($"{day:D}");
+    Console.WriteLine($"{day:d}");
+
+    int i = 2477;
+    Console.WriteLine($"{i:n} {i:e} {i:x} {i:c}");
+
+    double d = 3.1415;
+    Console.WriteLine($"{d:###.###}");
+    Console.WriteLine($"{d:000.000}");
+    Console.WriteLine();
+
+}
+UseStringFormat();
