a5963292b3b8281ab044f403e0a0ed7448815acb
小离 <438011578@qq.com>
2023-08-01 21:38:44 +0800

    p71

---
+++ 1_CS/chapter03-Types/MethodSample/MethodSample/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+class GenericMethods
+{
+    public static void Swap<T>(ref T x, ref T y)
+    {
+        // (x, y) = (y, x);
+        T t;
+        t = x;
+        x = y;
+        y = t;
+    }
+}
