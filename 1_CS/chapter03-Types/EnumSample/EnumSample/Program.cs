3c32503491f0880485fb0223b9bab2a12c8d7dff
小离 <438011578@qq.com>
2023-08-03 00:07:25 +0800

    Update Program.cs

--- 1_CS/chapter03-Types/EnumSample/EnumSample/Program.cs
+++ 1_CS/chapter03-Types/EnumSample/EnumSample/Program.cs
@@ -1,3 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+void ColorSamples()
+{
+    Color c1 = Color.Red;
+    Console.WriteLine(c1);
+}
+ColorSamples();
 
+Color1 c2 = (Color1)2;
+short number = (short)c2;
+Console.WriteLine(c2);
+Console.WriteLine(number);
+
+
+public enum Color
+{
+    Red,
+    Green,
+    Blue,
+}
+
+public enum Color1 : short
+{
+    Red = 1,
+    Green = 2,
+    Blue = 3
+}
