24e245550abf9bc6c9d2acd277f76838c63267da
小离 <438011578@qq.com>
2023-08-05 18:10:35 +0800

    p95

---
+++ 1_CS/chapter04-ObjectOrientation/VirtualMdthods/VirtualMdthods/ConcreteShape.cs
@@ -0,0 +1,53 @@
+﻿public class ConcreteShape : Shape
+{
+    protected override void DisplayShape()
+    {
+        //base.DisplayShape();
+        Console.WriteLine($"Rectangle at position {Position} with size {Size}.");
+    }
+}
+
+public class Rectangle : Shape
+{
+    public override Rectangle Clone()
+    {
+        Rectangle r = new();
+        r.Position.X = Position.X;
+        r.Position.Y = Position.Y;
+        r.Size.Width = Size.Width;
+        r.Size.Height = Size.Height;
+        return r;
+    }
+
+    public override void Move(Position newPosition)
+    {
+        Console.Write("Rectangle ");
+        base.Move(newPosition);
+    }
+}
+
+public class Ellipse : Shape
+{
+    protected override void DisplayShape()
+    {
+        Console.WriteLine($"Ellipse at position {Position} with size {Size}");
+    }
+
+    public override void Move(Position newPosition)
+    {
+        Console.Write("Ellipse ");
+        base.Move(newPosition);
+    }
+
+    public override Ellipse Clone()
+    {
+        Ellipse e = new();
+        e.Position.X = Position.X;
+        e.Position.Y = Position.Y;
+        e.Size.Width = Size.Width;
+        e.Size.Height = Size.Height;
+        return e;
+    }
+}
+
+
