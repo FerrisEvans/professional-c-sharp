64e148b6c96045524aedcdeeff7a8d2edaf065d7
小离 <438011578@qq.com>
2023-08-08 23:39:00 +0800

    p132

---
+++ 1_CS/chapter05-OperatorsAndCasts/OperatorOverloadingSample/OperatorOverloadingSample/Vector.cs
@@ -0,0 +1,22 @@
+﻿public class Vector
+{
+    public readonly double X;
+    public readonly double Y;
+    public readonly double Z;
+
+    public Vector(double x, double y, double z) => (X, Y, Z) = (x, y, z);
+
+    public Vector(Vector v) => (X, Y, Z) = (v.X, v.Y, v.Z);
+
+    public override string ToString() => $"Vector: x: {X}, y: {Y}, z: {Z}";
+
+    public static Vector operator +(Vector left, Vector right) => new Vector(left.X + right.X, left.Y + right.Y, left.Z + right.Z);
+
+    public static Vector operator -(Vector left, Vector right) => new Vector(left.X - right.X, left.Y - right.Y, left.Z - right.Z);
+
+    public static Vector operator *(Vector left, Vector right) => new Vector(left.X * right.X, left.Y * right.Y, left.Z * right.Z);
+
+    public static Vector operator *(double left, Vector right) => new Vector(left * right.X, left * right.Y, left * right.Z);
+
+    public static Vector operator *(Vector left, double right) => right * left;
+}
