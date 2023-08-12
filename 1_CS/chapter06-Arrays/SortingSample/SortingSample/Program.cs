696a5b7ed839b33506c6fa75a3bedb2e42c5450a
小离 <438011578@qq.com>
2023-08-13 01:45:03 +0800

    p156

---
+++ 1_CS/chapter06-Arrays/SortingSample/SortingSample/Program.cs
@@ -0,0 +1,14 @@
+﻿var persons = new Person[]
+{
+    new("Damon", "Hill"),
+    new("Niki", "Lauda"),
+    new("Ayrton", "Senna"),
+    new("Graham", "Hill"),
+};
+
+Array.Sort(persons);
+foreach(var p in persons) Console.WriteLine(p);
+
+Console.WriteLine($"-------");
+Array.Sort(persons, new PersonCompare(PersonCompareType.FirstName));
+foreach(var p in persons) Console.WriteLine(p);
