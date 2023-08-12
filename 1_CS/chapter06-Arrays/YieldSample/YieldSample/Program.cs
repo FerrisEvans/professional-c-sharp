e5b85a2e41964a4aa0267583d6f9099debb03646
小离 <438011578@qq.com>
2023-08-13 02:06:45 +0800

    p160

---
+++ 1_CS/chapter06-Arrays/YieldSample/YieldSample/Program.cs
@@ -0,0 +1,20 @@
+﻿var titles = new MusicTitles();
+
+foreach (var title in titles)
+{
+    Console.WriteLine($"{title}");
+}
+Console.WriteLine();
+
+Console.WriteLine("Reverse");
+foreach (var title in titles.Reverse())
+{
+    Console.WriteLine($"reverse: {title}");
+}
+Console.WriteLine();
+
+Console.WriteLine("Subset");
+foreach (var title in titles.Subset(2, 2))
+{
+    Console.WriteLine($"Subset: {title}");
+}
