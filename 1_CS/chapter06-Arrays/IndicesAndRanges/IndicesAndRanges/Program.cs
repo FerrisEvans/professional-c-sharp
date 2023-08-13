f5ef6ad649ef43877d242cb5e42a5441b5b3ca3a
小离 <438011578@qq.com>
2023-08-13 15:56:19 +0800

    p165

---
+++ 1_CS/chapter06-Arrays/IndicesAndRanges/IndicesAndRanges/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] data = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+int first1 = data[0];
+int last1 = data[data.Length - 1];
+Console.WriteLine($"first: {first1}, last: {last1}");
+
+int last2 = data[^1];
+Console.WriteLine(last2);
+
+Index fisrtIndex = 0;
+Index lastIndex = ^1;
+int first3 = data[fisrtIndex];
+int last3 = data[lastIndex];
+Console.WriteLine($"first: {first3}, last: {last3}");
+
+MyCollection coll = new();
+int n = coll[^20];
+Console.WriteLine($"Item from the collection: {n}");
+ShowRange("Using custom collection", coll[45..^40]);
+
+void ShowRange(string title, int[] data)
+{
+    Console.WriteLine(title);
+    Console.WriteLine(string.Join(" ", data));
+    Console.WriteLine($"");
+}
