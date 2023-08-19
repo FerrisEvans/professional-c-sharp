3f99c6c5d003d5d2fd3819a9f985225a9eb3d46b
小离 <438011578@qq.com>
2023-08-19 23:42:22 +0800

    p185

---
+++ 1_CS/chapter07-Dlegates/EventsSample/EventsSample/CarDealer.cs
@@ -0,0 +1,18 @@
+public class CarInfoEventArgs : EventArgs
+{
+    public CarInfoEventArgs(string car) => Car = car;
+    public string Car { get; }
+}
+
+public class CarDealer
+{
+    public event EventHandler<CarInfoEventArgs>? NewCarCreated;
+
+    public void CreateANewCar(string car)
+    {
+        Console.WriteLine($"CarDealer, new car {car}");
+        RaiseNewCarCreated(car);
+    }
+    
+    private void RaiseNewCarCreated(string car) => NewCarCreated?.Invoke(this, new CarInfoEventArgs(car));
+}
---
+++ 1_CS/chapter07-Dlegates/EventsSample/EventsSample/Program.cs
@@ -0,0 +1,10 @@
+﻿CarDealer dealer = new();
+Consumer sebastiam = new ("Sebastian");
+dealer.NewCarCreated += sebastiam.NewCarIsHere;
+dealer.CreateANewCar("Williams");
+
+Consumer max = new("Max");
+dealer.NewCarCreated += max.NewCarIsHere;
+dealer.CreateANewCar("Aston Martin");
+dealer.NewCarCreated -= sebastiam.NewCarIsHere;
+dealer.CreateANewCar("Ferrari");
