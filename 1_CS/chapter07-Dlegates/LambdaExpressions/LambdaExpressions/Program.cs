cc46ff1da1e0d6d75610ea2321694a5667710846
小离 <438011578@qq.com>
2023-08-19 22:33:44 +0800

    p182

---
+++ 1_CS/chapter07-Dlegates/LambdaExpressions/LambdaExpressions/Program.cs
@@ -0,0 +1,11 @@
+﻿Func<string, string> oneParam = s => $"change upper case {s.ToUpper()}";
+Console.WriteLine(oneParam("test"));
+
+Func<double, double, double> twoParam = (x, y) => x * y;
+Console.WriteLine(twoParam(3, 2));
+
+int val = 5;
+Func<int, int> f = x => x + val;
+Console.WriteLine(f(3)); // 8
+val = 7;
+Console.WriteLine(f(3)); // 10
