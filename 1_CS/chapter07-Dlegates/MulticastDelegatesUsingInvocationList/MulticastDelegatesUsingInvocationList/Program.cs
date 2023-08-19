9373ae56acea94d2cbfa598e799343ab93128e99
小离 <438011578@qq.com>
2023-08-19 21:29:29 +0800

    p179

---
+++ 1_CS/chapter07-Dlegates/MulticastDelegatesUsingInvocationList/MulticastDelegatesUsingInvocationList/Program.cs
@@ -0,0 +1,41 @@
+﻿WillOccurException();
+Action d = One;
+d += Two;
+var delegates = d.GetInvocationList();
+foreach (var @delegate in delegates)
+{
+    var de = (Action)@delegate;
+    try
+    {
+        de();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Exception caught");
+    }
+}
+
+void WillOccurException()
+{
+    Action d1 = One;
+    d1 += Two;
+    try
+    {
+        d1();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Exception caught");
+    }
+}
+
+static void One()
+{
+    Console.WriteLine("One");
+    throw new Exception("Error in One");
+}
+
+static void Two()
+{
+    Console.WriteLine($"Two");
+}
