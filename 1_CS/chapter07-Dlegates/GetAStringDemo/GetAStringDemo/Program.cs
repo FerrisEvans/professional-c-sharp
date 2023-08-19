c20a702bbf10d733c8c6681c40b4c867dcfdcfb7
小离 <438011578@qq.com>
2023-08-19 15:42:57 +0800

    p175

---
+++ 1_CS/chapter07-Dlegates/GetAStringDemo/GetAStringDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿int x = 40;
+GetAString firstStringMethod = x.ToString;
+Console.WriteLine($"String is {firstStringMethod()}");
+
+var balance = new Currency(34, 50);
+firstStringMethod = balance.ToString;
+Console.WriteLine($"String is {firstStringMethod()}");
+
+firstStringMethod = Currency.GetCurrencyUint;
+Console.WriteLine($"String is {firstStringMethod()}");
+
+public delegate string GetAString();
