810af642f0557d2522f03f04b8694e0b6c8260bc
小离 <438011578@qq.com>
2023-08-19 18:26:16 +0800

    p178

---
+++ 1_CS/chapter07-Dlegates/MulticastDelegates/MulticastDelegates/Program.cs
@@ -0,0 +1,14 @@
+﻿Action<double> operation1 = MathOperations.MultiplyByTwo;
+Action<double> operation2 = MathOperations.Square;
+Action<double> operations = operation1 + operation2;
+
+ProcessAndDisplayNumber(operations, 2.0);
+ProcessAndDisplayNumber(operations, 7.94);
+ProcessAndDisplayNumber(operations, 1.414);
+
+static void ProcessAndDisplayNumber(Action<double> action, double value)
+{
+    Console.WriteLine($"ProcessAndDisplayNumber called with value = {value}");
+    action(value);
+    Console.WriteLine();
+}
