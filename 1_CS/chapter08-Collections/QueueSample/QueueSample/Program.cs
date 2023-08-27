9b1e62fb90db9cb1ed6ec7d6c2b4c1d51e8a56c0
小离 <438011578@qq.com>
2023-08-27 23:16:47 +0800

    p199

---
+++ 1_CS/chapter08-Collections/QueueSample/QueueSample/Program.cs
@@ -0,0 +1,16 @@
+﻿DocumentManager dm = new();
+
+Task processDocuments = ProcessDocuments.StartAsync(dm);
+
+// Create documents and add them to the DocumentManager
+Random random = new();
+for (int i = 0; i < 1000; i++)
+{
+    var doc = new Document($"Doc {i}", "content");
+    int queueSize = dm.AddDocument(doc);
+    Console.WriteLine($"Added document {doc.Title}, queue size: {queueSize}");
+    await Task.Delay(random.Next(20));
+}
+Console.WriteLine($"finished adding documents");
+await processDocuments;
+Console.WriteLine("bye!");
