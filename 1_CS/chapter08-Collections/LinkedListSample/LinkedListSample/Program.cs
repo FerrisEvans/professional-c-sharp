6e2cd759c9a964643634943e566fe992ba048ecd
小离 <438011578@qq.com>
2023-08-31 15:02:59 +0800

    p202

---
+++ 1_CS/chapter08-Collections/LinkedListSample/LinkedListSample/Program.cs
@@ -0,0 +1,13 @@
+﻿var list = new LinkedList<Document>();
+LinkedListNode<Document> first = list.AddFirst(new Document(1, "first"));
+list.AddAfter(first, new Document(2, "second"));
+LinkedListNode<Document> last = list.AddLast(new Document(3, "last"));
+Document d = new(4, "before last");
+list.AddBefore(last, d);
+
+foreach (var document in list)
+{
+    Console.WriteLine($"{document}");
+}
+
+public record Document(int Id, string Text);
