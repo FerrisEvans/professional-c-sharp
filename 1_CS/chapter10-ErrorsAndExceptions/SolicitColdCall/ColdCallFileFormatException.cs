96a7407c195568351604dea84ca274f31a0b298c
小离 <438011578@qq.com>
2023-09-04 23:52:51 +0800

    p273

---
+++ 1_CS/chapter10-ErrorsAndExceptions/SolicitColdCall/ColdCallFileFormatException.cs
@@ -0,0 +1,8 @@
+public class ColdCallFileFormatException : Exception
+{
+    public ColdCallFileFormatException(string message)
+        : base(message) {}
+
+    public ColdCallFileFormatException(string message, Exception innerException)
+        : base(message, innerException) {}
+}
---
+++ 1_CS/chapter10-ErrorsAndExceptions/SolicitColdCall/UnexpectedException.cs
@@ -0,0 +1,12 @@
+public class UnexpectedException : Exception
+{
+    public UnexpectedException(string message)
+        : base(message)
+    {
+    }
+
+    public UnexpectedException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
