02709c8d61d6dd64b0e0f92825febee7339adec2
小离 <438011578@qq.com>
2023-09-04 23:33:38 +0800

    p266

---
+++ 1_CS/chapter10-ErrorsAndExceptions/ReshrowExceptions/MyCustomException.cs
@@ -0,0 +1,13 @@
+public class MyCustomException : Exception
+{
+    public MyCustomException(string message)
+        : base(message) { }
+
+    public int ErrorCode { get; set; }
+}
+
+public class AnotherCustomException : Exception
+{
+    public AnotherCustomException(string message, Exception innerException)
+        : base(message, innerException) { }
+}
